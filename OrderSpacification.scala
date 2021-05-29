8a9930334766b881e1271719e4226c3c74bcfae9
PulyaPolya <polinaitsme@gmail.com>
2021-05-29 09:04:02 +0300

    modified code

--- OrderSpacification.scala
+++ OrderSpacification.scala
@@ -1,18 +1,20 @@
 import org.scalacheck.Properties
 import org.scalacheck.Prop.forAll
 
-object StringSpecification extends Properties("String") {
+object OrderSpecification extends Properties("Order") {
 
-  property("startsWith") = forAll { (a: String, b: String) =>
-    (a+b).startsWith(a)
-  }
-
-  property("concatenate") = forAll { (a: String, b: String) =>
-    (a+b).length > a.length && (a+b).length > b.length
-  }
-
-  property("substring") = forAll { (a: String, b: String, c: String) =>
-    (a+b+c).substring(a.length, a.length+b.length) == b
+   val reflexive = forAll{
+        (a:Boolean, b:Boolean)=> (a<=a) == (true)
+      }
+      val transitive= forAll{
+        (a:Boolean,b:Boolean,c:Boolean)=>((a<=b)&&(b<=c))--> (a<=c)
+      }
+      val antisymmetric = forAll{
+        (a:Boolean, b:Boolean)=>((a<=b)&&(b<=a))--> (b=a)
+      }
+      val strong = forAll{
+        (a:Boolean, b:Boolean)=>(a<=b)||(b<=a)
+      }
   }
 
 }
